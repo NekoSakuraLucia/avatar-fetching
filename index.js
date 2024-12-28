0ccfd3da9fbf4b5ea0209958c4af04f3d6527d15
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-28 12:02:52 +0700

    Add basic code for creating endpoints.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// init code
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("Avatar Fetching");
+});
+
+app.listen(port, () => {
+    console.log(`Server Running on port ${port}`);
+});
